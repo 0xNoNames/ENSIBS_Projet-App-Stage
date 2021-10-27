e6b5e2050d36b7fd00ddbf491c166edf6d68d0f7
Arthur Pêtre <arthur30700@gmail.com>
2021-10-27 02:38:46 +0200

    Update UI
    Saturate & bg-black increased in the nav bar to help reading.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,13 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            backdropSaturate: {
+                "125": '1.25'
+            },
             colors: {
                 green: {
                     "ensibs-light": "#dee686",
-                    ensibs: "#a9c332",
+                    "ensibs": "#a9c332",
                     "ensibs-dark": "#608a3c"
                 }
             }
