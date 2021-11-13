6b4039f9c1771b06f4aa2fb9069b5f67f86450d4
Provot <simon.provot12@gmail.com>
2021-11-13 02:33:35 +0100

    Affichage nom prenom etuidants cvs

--- backend/controllers/routing/cvtheque.js
+++ backend/controllers/routing/cvtheque.js
@@ -1,16 +1,18 @@
 import CVModel from "../../models/cv.js";
+import CompteModel from "../../models/compte.js";
 
 
 export const cvPage = async (req,res) =>{
 	try {
 	    const Cvs = await CVModel.find();
+	    const comptes = await CompteModel.find();
 	    res.render("pages/cvtheque", {
 	      estConnecte: true,
 	      page: "CVthèque",
-	      username: req.compte.prenom+"_"+req.compte.nom,
 	      cvs: Cvs,
+	      comptes : comptes,
 	      prenom:req.compte.prenom,
-	      userid:req.compte.id
+	      
 	});
   } catch (error) {
     res.status(404).json({ message: error.message });
