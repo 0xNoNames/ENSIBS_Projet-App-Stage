587b1a3cca46c7bf717472f4733bd5726ab57af5
Franciscocoo <francois.jullion99@gmail.com>
2021-11-13 23:36:12 +0100

    Je renonce au backend

--- backend/controllers/routing/soutenance.js
+++ backend/controllers/routing/soutenance.js
@@ -1,26 +1,12 @@
 import SoutenanceModel from "../../models/soutenance.js";
-import { Calendar } from "@fullcalendar/core";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
-
-let calendar = new Calendar(calendar, {
-  plugins: [dayGridPlugin, listPlugin],
-  initialView: "dayGridMonth",
-  headerToolbar: {
-    left: "prev,next today",
-    center: "title",
-    right: "dayGridMonth,listWeek",
-  },
-});
 
 var lieux = [{ nom: "D0010" }];
 
 export const getSoutenancesPage = async (req, res) => {
   res.render("pages/soutenances", {
-    estConnecte: true,
-    page: "Soutenances de stage",
-    prenom: req.compte.prenom,
-    lieux: lieux,
+  estConnecte: true,
+  page: "Soutenances de stage",
+  prenom: req.compte.prenom,
+  lieux: lieux,
   });
 };
