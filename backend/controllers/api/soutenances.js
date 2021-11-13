1a0971d9954d8224575590a664b3de676183874a
Provot <simon.provot12@gmail.com>
2021-11-13 21:18:02 +0100

    Upload soutenances DB

--- backend/controllers/api/soutenances.js
+++ backend/controllers/api/soutenances.js
@@ -1,16 +1,50 @@
 import SoutenanceModel from "../../models/soutenance.js";
+import CompteModel from "../../models/compte.js";
 
 export const getSoutenances = (req, res) => {
   // var base_dir = path.resolve("./");
   // res.sendFile(base_dir + "/frontend/soutenances.html");
 };
 
-export const createSoutenance = (req, res) => {
-  // Get the value on name in the headers
+export const createSoutenance = async (req, res) => {
+  // Get the values in the requests
   var date = req.body.date;
   var lieu = req.body.lieu;
-  var hour = req.body.hour
+  var hour = req.body.hour;
 
+  var id = req.compte.id;
+  var email = req.compte.email;
+
+
+
+  // Create the Date Object
+  var year = date.slice(0,4);
+  var monthIndex = date.slice(5,7);
+  var day = date.slice(9,10);
+  var hours = hour.slice(0,2);
+  var minutes = hour.slice(3,5)
+  var date = new Date(year, monthIndex, day, hours, minutes)
+
+
+
+
+
+  try{
+    // verifier si le compte existe
+    const mongoCompte = await CompteModel.findOne({ email });
+
+    if (!mongoCompte){
+      res.status(400).json({msg :"Compte non trouvé"})
+    } else {
+      const cv = await SoutenanceModel.create({id_organisateur : id,date:date,lieu:lieu});
+      console.log("Le soutenance a bien ete upload")
+
+      res.status(200).json({msg : "Le CV a bien ete upload"})
+    }
+  } catch(erreur){
+    console.log(erreur)
+    res.status(400);
+  }
 
 
   /*
